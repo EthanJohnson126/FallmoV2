498a6c2fb9587e2bb175a689b7b55dbc0d0d5ccb
EthanJohnson126 <screwattackmoviemasters@gmail.com>
2015-04-14 21:25:20 -0500

    Falmo V4

---
+++ FallmoV4/BasicMechanics/Assets/TurnonPhysics.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurnonPhysics : MonoBehaviour {
+
+	//The objects to be checked and changed with rigidbodies.
+	public GameObject[] physicsObjects;
+
+	public float secondstoWait = 0;
+	public float secondstoFall = 0;
+
+	void Update () {
+	
+	}
+
+	void OnWithPhysics(){
+
+        //Lock mouse so the player can't rapidly click through the puzzle.
+        Screen.showCursor = false;
+        Screen.lockCursor = true;
+
+		//Sets all physics objects to be checked by the script.
+		physicsObjects = GameObject.FindGameObjectsWithTag("Physicsobject");
+
+		//Changes the physics to active.
+		foreach(GameObject physics in physicsObjects){
+			physics.rigidbody.isKinematic = false;
+			physics.rigidbody.useGravity = true;
+		}
+
+		//Returns physics to stable state.
+		Invoke("TurnoffPhysics", secondstoFall);
+	}
+
+	void TurnoffPhysics(){
+		//Turns off physics
+
+		foreach(GameObject physics in physicsObjects){
+			physics.rigidbody.isKinematic = true;
+			physics.rigidbody.useGravity = false;
+		}
+
+        //Turn the mouse back on.
+        Screen.showCursor = true;
+        Screen.lockCursor = false;
+	}
+
+	public void StartPhysics(){
+		//Calls physics change upon click.
+		Invoke("OnWithPhysics", secondstoWait);
+	}
+}
---
+++ FallmoV4/BasicMechanics/Assets/UIManager.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+
+public class UIManager : MonoBehaviour {
+
+    public GUISkin NoahsGUISkin;
+    public Texture2D aBackground, aLogo;
+
+    public Rect guiWindow = new Rect(Screen.width / 2, Screen.height / 2, 100, 100);
+
+    public string[] creditsText = new string[0];
+
+    private string menuState;
+
+    private string main = "main";
+    private string options = "options";
+    private string credits = "credits";
+
+    private string texttodisplay = "Credits \n";
+
+    private float volume = 1.0F;
+	// Use this for initialization
+	void Start () {
+
+        menuState = "main";
+
+        for (int x = 0; x > creditsText.Length; x++ )
+        {
+            texttodisplay += creditsText[x] + "\n";
+        }
+
+        texttodisplay += "Press Esc to go back";
+	}
+
+    public void OnGUI(){
+        if (aBackground != null){
+            GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), aBackground);
+        }
+
+        if (aLogo != null)
+        {
+            GUI.DrawTexture(new Rect((Screen.width / 3) - 100, 30, 200, 200), aLogo);
+        }
+
+        GUI.skin = NoahsGUISkin;
+
+        if (menuState == main){
+            guiWindow = GUI.Window(0, guiWindow, menuFunc, "gg");
+        }
+
+        if (menuState == credits)
+        {
+            GUI.Box(new Rect(0, 0, Screen.width, Screen.height), texttodisplay);
+        }
+
+        if (menuState == options)
+        {
+            guiWindow = GUI.Window(1, guiWindow, optionsFunc, "Options");
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (menuState == credits && Input.GetKey(KeyCode.Escape)){
+            menuState = main;
+        }
+	}
+
+    private void optionsFunc(int id){
+        GUILayout.Box("Volume");
+        volume = GUILayout.HorizontalSlider(volume, 0.0F, 1.0F);
+        AudioListener.volume = volume;
+
+        if (GUILayout.Button("Back"))
+        {
+            menuState = main;
+        }
+    }
+
+    private void menuFunc (int id){
+        if (GUILayout.Button("Play Game")){
+            print("tttt");
+        }
+
+        if (GUILayout.Button("Options"))
+        {
+            menuState = options;
+        }
+
+        if (GUILayout.Button("Credits"))
+        {
+            menuState = credits;
+        }
+    }
+}
